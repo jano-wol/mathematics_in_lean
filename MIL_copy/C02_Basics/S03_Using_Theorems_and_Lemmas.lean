9aca2ad649b8cf3dfc5f55af9311da78c1a95aeb
Andrzej Polak <jjjkamu@gmail.com>
2024-10-04 09:47:20 +0200

    C01 S03

--- MIL_copy/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ MIL_copy/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -44,7 +44,10 @@ example (x : ℝ) : x ≤ x :=
 
 -- Try this.
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
-  sorry
+  apply lt_of_le_of_lt h₀
+  apply lt_of_lt_of_le h₁
+  apply le_trans h₂
+  apply le_of_lt h₃
 
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
   linarith
@@ -86,21 +89,37 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
     apply exp_lt_exp.mpr h₁
   apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  apply add_le_add_left
+  apply h₀
+
 
 example : (0 : ℝ) < 1 := by norm_num
 
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
+  have h₀ : 0 < 1 + exp a := by
+    apply add_pos
+    . norm_num
+    . apply exp_pos
   apply log_le_log h₀
-  sorry
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  apply h
 
 example : 0 ≤ a ^ 2 := by
   -- apply?
   exact sq_nonneg a
 
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  apply sub_le_sub
+  . apply le_refl
+  . apply exp_le_exp.mpr
+    apply h
+
+example (h : a ≤ b) : c - exp b ≤ c - exp a := by
+  linarith [exp_le_exp.mpr h]
 
 example : 2 * a * b ≤ a ^ 2 + b ^ 2 := by
   have h : 0 ≤ a ^ 2 - 2 * a * b + b ^ 2
@@ -110,7 +129,7 @@ example : 2 * a * b ≤ a ^ 2 + b ^ 2 := by
 
   calc
     2 * a * b = 2 * a * b + 0 := by ring
-    _ ≤ 2 * a * b + (a ^ 2 - 2 * a * b + b ^ 2) := add_le_add (le_refl _) h
+    _ ≤ 2 * a * b + (a ^ 2 - 2 * a * b + b ^ 2) := add_le_add (le_refl _) (h)
     _ = a ^ 2 + b ^ 2 := by ring
 
 example : 2 * a * b ≤ a ^ 2 + b ^ 2 := by
@@ -121,7 +140,23 @@ example : 2 * a * b ≤ a ^ 2 + b ^ 2 := by
   linarith
 
 example : |a * b| ≤ (a ^ 2 + b ^ 2) / 2 := by
-  sorry
+  apply abs_le'.mpr
+  have h : 0 ≤ (a - b) ^ 2 := by
+    apply pow_two_nonneg
+  have h2 : 0 ≤ (a - b) ^ 2 / 2 := by
+    linarith
+  have h3 : 0 ≤ (a + b) ^ 2 := by
+    apply pow_two_nonneg
+  have h4 : 0 ≤ (a + b) ^ 2 / 2 := by
+    linarith
+  constructor
+  . calc
+      a * b = a * b + 0 := by ring
+      _ ≤ a * b + (a - b) ^ 2 / 2 := add_le_add (le_refl _) (h2)
+      _ = (a ^ 2 + b ^ 2) / 2 := by ring
+  . calc
+      -(a * b) = -(a * b) + 0 := by ring
+      _ ≤ -(a * b) + (a + b) ^ 2 / 2 := add_le_add (le_refl _) (h4)
+      _ = (a ^ 2 + b ^ 2) / 2 := by ring
 
 #check abs_le'.mpr
-
