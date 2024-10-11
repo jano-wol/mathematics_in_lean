34e86369e51e98fcfa634e9e9df788487e5675aa
janowol <jjjkamu@gmail.com>
2024-10-11 22:15:41 +0200

    C03 S03 ready

--- MIL_copy/C03_Logic/S03_Negation.lean
+++ MIL_copy/C03_Logic/S03_Negation.lean
@@ -66,34 +66,59 @@ example (h : Monotone f) (h' : f a < f b) : a < b := by
   linarith
 
 example (h : a ≤ b) (h' : f b < f a) : ¬Monotone f := by
-  sorry
+  intro n
+  have h2 := n h
+  linarith
 
 example : ¬∀ {f : ℝ → ℝ}, Monotone f → ∀ {a b}, f a ≤ f b → a ≤ b := by
   intro h
-  let f := fun x : ℝ ↦ (0 : ℝ)
-  have monof : Monotone f := by sorry
+  let f := fun _ : ℝ ↦ (0 : ℝ)
+  have monof : Monotone f := by
+    dsimp [Monotone]
+    intro p q _
+    linarith
   have h' : f 1 ≤ f 0 := le_refl _
-  sorry
+  have h2 : ∀ {a b : ℝ}, f a ≤ f b → a ≤ b := h monof
+  have h3 : (1 : ℝ)  ≤ 0 := h2 h'
+  linarith
 
 example (x : ℝ) (h : ∀ ε > 0, x < ε) : x ≤ 0 := by
-  sorry
-
+  apply le_of_not_gt
+  intro n
+  have h1 : 0 < (x / 2) := by linarith[n]
+  have h2 := h (x / 2) h1
+  linarith
 end
 
 section
 variable {α : Type*} (P : α → Prop) (Q : Prop)
 
 example (h : ¬∃ x, P x) : ∀ x, ¬P x := by
-  sorry
+  intro x
+  intro n
+  have h2 : ∃ x, P x := by
+    use x
+  contradiction
 
 example (h : ∀ x, ¬P x) : ¬∃ x, P x := by
-  sorry
+  intro n
+  obtain ⟨x, hx⟩ := n
+  have h2 := h x
+  contradiction
 
 example (h : ¬∀ x, P x) : ∃ x, ¬P x := by
-  sorry
+  by_contra h'
+  apply h
+  intro x
+  show P x
+  by_contra h''
+  exact h' ⟨x, h''⟩
 
 example (h : ∃ x, ¬P x) : ¬∀ x, P x := by
-  sorry
+  intro n
+  obtain ⟨x, hx⟩ := h
+  have h2 := n x
+  contradiction
 
 example (h : ¬∀ x, P x) : ∃ x, ¬P x := by
   by_contra h'
@@ -104,10 +129,12 @@ example (h : ¬∀ x, P x) : ∃ x, ¬P x := by
   exact h' ⟨x, h''⟩
 
 example (h : ¬¬Q) : Q := by
-  sorry
+  by_contra h'
+  contradiction
 
 example (h : Q) : ¬¬Q := by
-  sorry
+  intro n
+  contradiction
 
 end
 
@@ -115,7 +142,25 @@ section
 variable (f : ℝ → ℝ)
 
 example (h : ¬FnHasUb f) : ∀ a, ∃ x, f x > a := by
-  sorry
+  have h2 : ∀ a, ¬∀ (x : ℝ), f x ≤ a := by
+    intro x
+    intro n
+    have h2 : ∃ a, ∀ (x : ℝ), f x ≤ a := by
+      use x
+    contradiction
+  have h3 : ∀ a, ∃ (x : ℝ), ¬f x ≤ a := by
+    intro a
+    have  h3 := h2 a
+    by_contra h'
+    apply h3
+    intro x
+    by_contra h''
+    exact h' ⟨x, h''⟩
+  intro a
+  obtain ⟨x, hx⟩ := h3 a
+  use x
+  exact not_le.mp hx
+
 
 example (h : ¬∀ a, ∃ x, f x > a) : FnHasUb f := by
   push_neg at h
@@ -127,7 +172,12 @@ example (h : ¬FnHasUb f) : ∀ a, ∃ x, f x > a := by
   exact h
 
 example (h : ¬Monotone f) : ∃ x y, x ≤ y ∧ f y < f x := by
-  sorry
+  dsimp [Monotone] at h
+  push_neg at h
+  obtain ⟨a, b, hab⟩ := h
+  obtain ⟨hab1, hab2⟩ := hab
+  use a, b
+
 
 example (h : ¬FnHasUb f) : ∀ a, ∃ x, f x > a := by
   contrapose! h
@@ -151,7 +201,7 @@ example (h : 0 < 0) : a > 37 :=
   absurd h (lt_irrefl 0)
 
 example (h : 0 < 0) : a > 37 := by
-  have h' : ¬0 < 0 := lt_irrefl 0
+  have _' : ¬0 < 0 := lt_irrefl 0
   contradiction
 
 end
